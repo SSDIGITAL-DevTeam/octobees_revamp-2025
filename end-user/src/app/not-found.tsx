42b995ac1db7a2cd79f3208057e64ce15732048f
ryankusuma <ryankusuma.atwork@gmail.com>
2025-05-16 11:41:22 +0700

    ryan : add meta pixel thanks - increase my sales

--- end-user/src/app/not-found.tsx
+++ end-user/src/app/not-found.tsx
@@ -18,7 +18,7 @@ export default function NotFound() {
         <h1 className="text-center md:text-left text-xl md:text-3xl lg:text-5xl font-semibold">Sorry, This Page Doesn’t Exist</h1>
         <p className="text-center md:text-left text-sm md:text-xl !leading-[130%] text-gray-600">the page you’re looking for doesn’t exist or may have been moved.</p>
         <Button onClick={() => router.push('/')} className="py-3 px-6 font-semibold rounded-xl lg:mt-0 mt-7 bg-red-700">Return to the Homepage</Button>
-        <div className="hidden absolute bottom-10 right-10 lg:flex flex-col items-end font-bold text-red-700 gap-8">
+        <div className="hidden absolute bottom-10 right-10 lg:flex flex-col items-end font-bold text-red-700 gap-8 md:max-w-[40vw]">
           <p className="text-5xl">Oops! Error</p>
           <Image src={NotFoundGif} alt="404" width={400} height={400} className="w-full" />
         </div>
---
+++ end-user/src/app/thanks/contact-us/page.tsx
@@ -0,0 +1,6 @@
+import ThanksPage from "../_components/thanksPage";
+
+export default function Page(): JSX.Element {
+  return <ThanksPage/>
+}
+
