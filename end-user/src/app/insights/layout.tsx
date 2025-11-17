7e4c6361571d6e49c8b9b542f7bbe66d006903de
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2025-11-17 11:16:02 +0700

    refactor: update insight/layout add meta

--- end-user/src/app/insights/layout.tsx
+++ end-user/src/app/insights/layout.tsx
@@ -3,20 +3,30 @@ import generateMetatag from '@/utils/generateMetadata';
 import { ReactNode } from 'react';
 
 export async function generateMetadata() {
-  const metaTag = pageMetadata.insights
+  const metaTag = pageMetadata.insights;
   const location = "insights";
-   try {
-    return await generateMetatag({ location, metaTag });
+
+  try {
+    const generated = await generateMetatag({ location, metaTag });
+
+    return {
+      ...generated,
+      other: {
+        ...generated.other,
+        "google-site-verification": "Om6R9r0thAlgOsy4-m-eBbYWAa8LTyxpeOTqnlTyNqc",
+      },
+    };
   } catch (error) {
     return {
       ...defaultMetadata,
       ...metaTag,
+      other: {
+        "google-site-verification": "Om6R9r0thAlgOsy4-m-eBbYWAa8LTyxpeOTqnlTyNqc",
+      },
     };
   }
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-      <>{children}</>
-  )
+  return <>{children}</>;
 }
