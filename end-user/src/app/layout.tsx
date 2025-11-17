18c875d315a69c9b532630ae55337bca67d57bd3
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2025-11-17 13:39:42 +0700

    refactor: update meta on app/layout

--- end-user/src/app/layout.tsx
+++ end-user/src/app/layout.tsx
@@ -3,15 +3,26 @@ import Navbar from "@/components/layouts/Navbar/Navbar";
 import Footer from "@/components/layouts/Footer/Footer";
 import Script from "next/script";
 import React from "react";
+import type { Metadata } from "next";
 import generateMetatag from "@/utils/generateMetadata";
 import { Providers } from "./providers";
 import { Toaster } from "@/components/ui/toaster";
 import { pageMetadata } from "@/constants/metadata";
 
-export async function generateMetadata() {
-  const metaTag = pageMetadata.home
+export async function generateMetadata(): Promise<Metadata> {
+  const metaTag = pageMetadata.home;
   const location = "home";
-  return await generateMetatag({ location, metaTag: metaTag });
+
+  const generated = await generateMetatag({ location, metaTag });
+
+  return {
+    ...generated,
+    other: {
+      ...(generated as any)?.other,
+      "google-site-verification":
+        "Om6R9r0thAlgOsy4-m-eBbYWAa8LTyxpeOTqnlTyNqc",
+    },
+  };
 }
 
 export default function RootLayout({
@@ -21,6 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
+      {/* Google Tag Manager */}
       <Script
         id="gtm"
         strategy="afterInteractive"
@@ -28,18 +40,22 @@ export default function RootLayout({
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W453F787');`,
         }}
       />
-      
+
       <html lang="en">
         <body className="font-body">
+          {/* GTM noscript */}
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W453F787" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           ></noscript>
+
           <Navbar />
+
           <Providers>
-            <>{children}</>
+            {children}
           </Providers>
+
           <Footer />
           <Toaster />
         </body>
