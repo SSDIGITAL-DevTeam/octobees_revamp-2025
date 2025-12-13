64b20bcde5c80e3335a69848ff65190bcc158efb
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2025-12-13 09:13:30 +0700

    partner: lengkapi auth client (useAuth, AuthGuard, login flow)

---
+++ src/lib/auth/constants.ts
@@ -0,0 +1,8 @@
+export const ACCESS_TOKEN_COOKIE = "accessToken";
+export const AUTH_REDIRECT_PARAM = "redirect";
+
+export const PUBLIC_AUTH_PATHS = [
+  "/login",
+  "/forgot-password",
+  "/affiliate/change-password",
+];
